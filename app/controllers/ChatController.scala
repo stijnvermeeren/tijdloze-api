1bc6823d4b75264f4619c279d01f96f297d460c2
stijn <stijn@stijnvermeeren.be>
2019-12-16 22:57:28 +0100

    trying to implement a chatMessagesSource

--- app/controllers/ChatController.scala
+++ app/controllers/ChatController.scala
@@ -1,100 +1,26 @@
 package controllers
 
-import akka.stream.Materializer
-import akka.stream.scaladsl.{BroadcastHub, Flow, Keep, MergeHub, Source}
 import util.Chat
 import javax.inject._
-import model.api.{ChatMessage, ChatSave, ChatTicket, PublicUserInfo}
-import model.db.dao.{ChatOnlineDAO, ChatTicketDAO}
+import model.api.ChatTicket
+import model.db.dao.ChatTicketDAO
 import play.api.libs.json._
 import play.api.mvc._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
-import scala.concurrent.duration._
 
 @Singleton
 class ChatController @Inject()(
   authenticate: Authenticate,
   chat: Chat,
-  chatOnlineDAO: ChatOnlineDAO,
   chatTicketDAO: ChatTicketDAO
-)(implicit mat: Materializer) extends InjectedController {
+) extends InjectedController {
 
   def ticket() = (Action andThen authenticate).async { implicit request =>
     chatTicketDAO.create(request.user.id) map { ticket =>
       Ok(Json.toJson(ChatTicket(ticket)))
     }
-  }
-
-  // chat room many clients -> merge hub -> broadcasthub -> many clients
-  private val (chatSink, chatSource) = {
-    // Don't log MergeHub$ProducerFailed as error if the client disconnects.
-    // recoverWithRetries -1 is essentially "recoverWith"
-    val source = MergeHub.source[JsValue]
-      .log("source")
-      .recoverWithRetries(-1, { case _: Exception => Source.empty })
-
-    val sink = BroadcastHub.sink[JsValue]
-    source.toMat(sink)(Keep.both).run()
-  }
-
-  // source for online list
-  Source
-    .tick(
-      initialDelay = 15.seconds,
-      interval = 15.seconds,
-      tick = ()
-    )
-    .mapAsync(1) { _ =>
-      chatOnlineDAO.list(maxAgeSeconds = 30) map { users =>
-        Json.toJson(
-          users map PublicUserInfo.fromDb
-        )
-      }
-    }
-    .to(chatSink)
-    .run()
-
-  private def chatFlow(userId: String): Flow[JsValue, JsValue, _] = {
-    val userSink = Flow[JsValue]
-      .mapAsync(2){ body =>
-        Json.fromJson[ChatSave](body).fold(
-          errors => {
-            Future.failed(throw new Exception("Invalid message"))
-          },
-          chatSave => {
-            // TODO use actual user id and display name
-            chat
-              .post(userId, chatSave.message)
-              .map(dbChatMessage => ChatMessage.fromDb(dbChatMessage, "DisplayName"))
-              .map(Json.toJson[ChatMessage])
-          }
-        )
-      }
-      .to(chatSink)
-
-    val userSource = chatSource.mapAsync(1) { message =>
-      // TODO don't save online status too often
-      chat.saveOnlineStatus(userId) map { _ =>
-        message
-      }
-    }
-
-    // initialisation
-    val initSource = Source
-      .single(())
-      .mapAsync(1) { _ =>
-        chatOnlineDAO.list(maxAgeSeconds = 30) map { users =>
-          Json.toJson(
-            users map PublicUserInfo.fromDb
-          )
-        }
-      }
-
-    Flow[JsValue]
-      .via(Flow.fromSinkAndSource(userSink, initSource.concat(userSource)))
-      .log("chatFlow")
   }
 
   def ws(): WebSocket = {
@@ -104,7 +30,7 @@ class ChatController @Inject()(
         case Some(ticket) =>
           chatTicketDAO.use(ticket) map {
             case Some(userId) =>
-              val flow = chatFlow(userId)
+              val flow = chat.chatFlow(userId)
               Right(flow)
             case None =>
               Left(Forbidden)
--- app/util/Chat.scala
+++ app/util/Chat.scala
@@ -1,50 +1,136 @@
 package util
 
+import akka.stream.Materializer
+import akka.stream.scaladsl.{BroadcastHub, Flow, Keep, MergeHub, Source}
 import javax.inject.{Inject, Singleton}
-import model.ChatMessageId
-import model.db.ChatMessage
+import model.api.{ChatMessage, ChatSave, PublicUserInfo}
+import model.db
 import model.db.dao.{ChatMessageDAO, ChatOnlineDAO}
 import play.api.Logger
+import play.api.libs.json.{JsValue, Json}
+import play.api.libs.json._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+import scala.concurrent.duration._
 
 @Singleton
-class Chat @Inject() (chatMessageDAO: ChatMessageDAO, chatOnlineDAO: ChatOnlineDAO, displayNames: DisplayNames) {
+class Chat @Inject() (
+  chatMessageDAO: ChatMessageDAO,
+  chatOnlineDAO: ChatOnlineDAO,
+  displayNames: DisplayNames
+)(implicit mat: Materializer) {
   val logger = Logger(getClass)
 
-  var messages: List[ChatMessage] = List.empty
-
-  def post(userId: String, message: String): Future[ChatMessage] = {
+  private def post(userId: String, message: String): Future[db.ChatMessage] = {
     saveOnlineStatus(userId)
 
     chatMessageDAO.save(userId, message) map { chatMessage =>
-      synchronized {
-        messages = chatMessage :: messages
-      }
       chatMessage
     }
   }
 
-  def get(userId: String, sinceId: Int): Future[List[(ChatMessage, String)]] = {
-    saveOnlineStatus(userId)
-
-    displayNames.get() map { displayNames =>
-      val responseMessages = if (sinceId > 0) {
-        messages.takeWhile(_.id != ChatMessageId(sinceId))
-      } else {
-        messages.take(20)
-      }
-      responseMessages.reverse map { message =>
-        (message, displayNames.getOrElse(message.userId, ""))
-      }
-    }
-  }
-
-  def saveOnlineStatus(userId: String): Future[Unit] = {
+  private def saveOnlineStatus(userId: String): Future[Unit] = {
     chatOnlineDAO.save(userId) recover {
       case e =>
         logger.error("Error while saving chat online status.", e)
     }
+  }
+
+
+  // chat room many clients -> merge hub -> broadcasthub -> many clients
+  private val (chatSink, chatSource) = {
+    // Don't log MergeHub$ProducerFailed as error if the client disconnects.
+    // recoverWithRetries -1 is essentially "recoverWith"
+    val source = MergeHub.source[ChatMessage]
+      .log("source")
+      .recoverWithRetries(-1, { case _: Exception => Source.empty })
+
+    val sink = BroadcastHub.sink[ChatMessage]
+    source.toMat(sink)(Keep.both).run()
+  }
+
+  // source for online list
+  private val onlineSource: Source[JsValue, _] = Source
+    .tick(
+      initialDelay = 15.seconds,
+      interval = 15.seconds,
+      tick = ()
+    )
+    .mapAsync(1) { _ =>
+      chatOnlineDAO.list(maxAgeSeconds = 30) map { users =>
+        Json.toJson(
+          users map PublicUserInfo.fromDb
+        )
+      }
+    }
+
+  val lastMessagesSource: Source[Seq[ChatMessage], _] = chatSource
+    .map{x =>
+      println("a " + x)
+      x
+    }
+    .scan(Seq.empty[ChatMessage]){
+      case (previous, newMessage) => previous.takeRight(1) :+ newMessage
+    }
+    .map{x =>
+      println("+ " + x)
+      x
+    }
+    .expand(Iterator.continually(_))
+    .map{x =>
+      println("- " + x)
+      x
+    }
+
+  private val lastMessagesJsonSource: Source[JsValue, _] = lastMessagesSource
+    .take(1)
+    .flatMapConcat(messages => { println(messages.map(_.message)); Source(messages.toList) })
+    .map(Json.toJson[ChatMessage])
+
+  private val chatJsonSource: Source[JsValue, _] = chatSource
+    .map(Json.toJson[ChatMessage])
+
+  def chatFlow(userId: String): Flow[JsValue, JsValue, _] = {
+    val userSink = Flow[JsValue]
+      .mapAsync(2){ body =>
+        Json.fromJson[ChatSave](body).fold(
+          errors => {
+            Future.failed(throw new Exception("Invalid message"))
+          },
+          chatSave => {
+            // TODO use actual user id and display name
+            post(userId, chatSave.message)
+              .map(dbChatMessage => ChatMessage.fromDb(dbChatMessage, "DisplayName"))
+          }
+        )
+      }
+      .to(chatSink)
+
+    val userOnlineSource: Source[JsValue, _] = onlineSource.mapAsync(1) { message =>
+      saveOnlineStatus(userId) map { _ =>
+        message
+      }
+    }
+
+    val userSource: Source[JsValue, _] = chatJsonSource merge userOnlineSource
+
+    // initialisation
+    val initSource = Source
+      .single(())
+      .mapAsync(1) { _ =>
+        chatOnlineDAO.list(maxAgeSeconds = 30) map { users =>
+          Json.toJson(
+            users map PublicUserInfo.fromDb
+          )
+        }
+      }
+
+    Flow[JsValue]
+      .via(Flow.fromSinkAndSource(
+        userSink,
+        initSource.concat(lastMessagesJsonSource).concat(userSource)
+      ))
+      .log("chatFlow")
   }
 }
