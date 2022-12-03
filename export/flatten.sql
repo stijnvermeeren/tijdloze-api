5db26677e5bc9823c127cf991539abaffcc4e338
stijnvermeeren <21952925+stijnvermeeren@users.noreply.github.com>
2022-12-03 17:41:38 +0100

    Add second artist to flatten.sql script

--- export/flatten.sql
+++ export/flatten.sql
@@ -12,7 +12,7 @@ SELECT
   ) INTO @sql
 FROM
   (SELECT DISTINCT year FROM list_entry ORDER BY year ASC) as years;
-SET @sql = CONCAT('SELECT s.id as song_id, s.title as song_title, a.id as artist_id, IFNULL(a.name_prefix, "") as name_prefix, a.name as name, c.id as album_id, c.title as album_title, c.release_year as release_year, ', @sql, ' FROM song s JOIN artist a ON s.artist_id = a.id JOIN album c ON s.album_id = c.id');
+SET @sql = CONCAT('SELECT s.id as song_id, s.title as song_title, a.id as artist_id, IFNULL(a.name_prefix, "") as name_prefix, a.name as name, IFNULL(a2.id, "") as second_artist_id, IFNULL(a2.name_prefix, "") as second_name_prefix, IFNULL(a2.name, "") as second_name, c.id as album_id, c.title as album_title, c.release_year as release_year, ', @sql, ' FROM song s JOIN artist a ON s.artist_id = a.id LEFT JOIN artist a2 ON s.second_artist_id = a2.id JOIN album c ON s.album_id = c.id');
 
 PREPARE stmt FROM @sql;
 EXECUTE stmt;
