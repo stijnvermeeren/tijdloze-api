3bb0c2287570e9bdd98e4444ac5c4ad550f1a338
stijnvermeeren <21952925+stijnvermeeren@users.noreply.github.com>
2025-04-04 22:27:24 +0200

    improved db export

--- export/export.sh
+++ export/export.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+mkdir -p tijdloze
+rm -r tijdloze/*
+cp -r static/* tijdloze/
+
 psql "$@" -t -A < flatten_query.sql | psql "$@" > tijdloze.tsv
 
-pg_dump "$@" --data-only -t artist -t album -t song -t year -t list_entry > tijdloze-data.sql
-pg_dump "$@" -t artist -t album -t song -t year -t list_entry > tijdloze-schema-data.sql
+echo "COPY (SELECT * FROM song) TO STDOUT CSV HEADER DELIMITER E'\t';" | psql "$@" -t -A > tijdloze/song.tsv
+echo "COPY (SELECT * FROM artist) TO STDOUT CSV HEADER DELIMITER E'\t';" | psql "$@" -t -A > tijdloze/artist.tsv
+echo "COPY (SELECT * FROM album) TO STDOUT CSV HEADER DELIMITER E'\t';" | psql "$@" -t -A > tijdloze/album.tsv
+echo "COPY (SELECT * FROM list_entry) TO STDOUT CSV HEADER DELIMITER E'\t';" | psql "$@" -t -A > tijdloze/list_entry.tsv
+echo "COPY (SELECT * FROM year) TO STDOUT CSV HEADER DELIMITER E'\t';" | psql "$@" -t -A > tijdloze/year.tsv
+
+pg_dump "$@" --schema-only -t artist -t album -t song -t list_entry -t year > tijdloze/postgres-schema.sql
+
+zip -r tijdloze.zip tijdloze/
