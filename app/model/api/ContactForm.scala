95abc8aa53a0782b9a04f3ac20934eb3debe7142
stijnvermeeren <21952925+stijnvermeeren@users.noreply.github.com>
2025-11-17 22:13:56 +0100

    contact: add debug recipients + add sender name to subject

--- app/model/api/ContactForm.scala
+++ app/model/api/ContactForm.scala
@@ -5,7 +5,8 @@ import play.api.libs.json.Json
 final case class ContactForm(
   name: String,
   email: Option[String],
-  message: String
+  message: String,
+  debug: Boolean = false
 )
 
 object ContactForm {
